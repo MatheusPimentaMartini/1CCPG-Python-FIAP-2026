e09c5d870a2e7b59a0fdc6113b338f0cb8f030c6
MatheusPimentaMartini <matheuspimenta576@gmail.com>
2026-09-17 11:46:29 -0300

    Aula_09

--- Aula08_mini_crm/app.py
+++ Aula08_mini_crm/app.py
@@ -15,21 +15,62 @@ def add_lead():
     # para salvar, vamos usar o módulo control
     control.create_lead(model_lead(name,email,company,step))
 
+    print("Lead adicionado (func)")
+
+def list_leads():
+    
+    leads = control.read_leads()
+    if not leads:
+        print("Nenhum lead ainda")
+        return
+
+    print(f'# | {"Nome": < 10} | {"E-mail" : < 10} | Empresa')
+    for i, lead in enumerate(leads):
+        print(f"{i:02d} | {lead["name"]: <10} | {lead["email"]:<10} | {lead["company"]}")
+
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    if not query:
+        print("Consulta vazia")
+        return
+
+    #envia a query para o control realizar a busca no leads.json
+    leads_finded = control.read_leads_search(query)
+
+    print(f'# | {"Nome":<10} | {"E-mail":<10} | Empresa')
+    for i, lead in leads_finded:
+        print(f"{i:02d} | {lead["Name"]:<10} | {lead["email"]:<10} | {lead["company"]}")
+
+
+def export_leads():
+    path_csv = control.export_csv()
+
+    if path_csv is None:
+        print("Não foi possivel exportar os leads")
+    else:
+        print(f"Exportado para {path_csv}")
+
 
 def main():
-    print("\nMIni CRM de Leads")
-    print("[1] Adicionar lead")
-    print("[2] Listar leads")
-    print("[0] Sair do programa")
-    
-    opt = input("Escolha uma oção: ")
     while True:
+        print("\nMIni CRM de Leads")
+        print("[1] Adicionar lead")
+        print("[2] Listar leads")
+        print("[3] Buscar (nome/e-mail/empresa)")
+        print("[4] Exportar CSV")
+        print("[0] Sair do programa")
+        
+        opt = input("Escolha uma oção: ")
         if opt == "1":
             add_lead()
         elif opt == "2":
-            print("Lista leads")
+            list_leads()
+        elif opt == "3":
+            search_leads()
+        elif opt == "4":
+            export_leads()
         elif opt == "0":
-            print("Até mais...")
+            
             break
         else:
             print("Opção inválida")
--- Aula08_mini_crm/control.py
+++ Aula08_mini_crm/control.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 print(DATA_DIR)
@@ -26,3 +26,33 @@ def create_lead(lead_dict):
     leads = read_leads() # Lista de dicionarios de leads
     leads.append(lead_dict)
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+
+# EXPORT LEADS COMO CSV
+def export_csv():
+    """Exporta os leads para CSV e RETORNA o caminho do arquivo criado"""
+    path_csv = DATA_DIR / "leads.csv"
+
+    leads = read_leads() #LISTA - ARRAY!!
+
+    try:
+        with path_csv.open("w", newline="", encoding="utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, fieldnames=leads[0].keys())
+            writer.writeheader()
+            for row in leads:
+                writer.writerow(row)
+        return path_csv
+    except PermissionError:
+        return None
+
+# READ LEADS FROM QUERY/SEARCH
+def read_leads_search(query):
+    leads = read_leads()
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["Name"]} {lead["email"]} {lead["company"]}".lower()
+
+        if query.lower() in txt_lead:
+            results.append((1, lead)) #(0, {"name": "Alexandre" ...})
+
+    return results
